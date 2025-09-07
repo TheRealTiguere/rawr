46a5c47b5198bf712b3824c8f6d5a83075bbb15c
TheRealTiguere <92551349+TheRealTiguere@users.noreply.github.com>
2025-09-07 03:23:22 +0200

    fix: Simplify Vercel config, add .vercelignore, update Dockerfile to Node 20

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
     serverComponentsExternalPackages: ['@prisma/client']
   },
   
-  // Optimisations SEO et performances
-  compress: true,
-  poweredByHeader: false,
-  
   // Images optimization
   images: {
     formats: ['image/webp', 'image/avif'],
